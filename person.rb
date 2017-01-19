7e8507adcca1c56fa0675c0a0850f4c8f9df8f52
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-19 12:54:38 -0500

    Create person, student and instructor class

---
+++ person.rb
@@ -0,0 +1,25 @@
+class Person
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
+class Student < Person
+
+  def learn
+    "I get it!"
+  end
+
+end
+
+class Instructor < Person
+
+  def teach
+    "Everything in Ruby is an Object"
+  end
+
+end
